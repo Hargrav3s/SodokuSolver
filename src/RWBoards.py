8616fd47682e8f41be5febd1920c937ee1c1fcf6
Tyler Hargraves <Harqrav3s@gmail.com>
2021-06-02 18:51:40 -0400

    Inital dev commit

---
+++ src/RWBoards.py
@@ -0,0 +1,12 @@
+import sys
+
+# Assume the given format is that described in the overview file as such:
+#   an ‘X’ represents an open cell and a number indicates a fixed starting value for a cell.
+
+# reads a sudoku board from the path location and returns it as a python list.
+def read_board(path):
+    pass
+
+# writes a sudoku board in python list format to a file specified by path.
+def write_board(board, path):
+    pass
